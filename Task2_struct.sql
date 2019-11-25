3858482f13109cfb30c5c57240578edd68aadecb
MarinaArh <38554014+MarinaArh@users.noreply.github.com>
2019-11-26 00:07:21 +0300

    Update

--- Task2_struct.sql
+++ Task2_struct.sql
@@ -1,15 +1,8 @@
--- Создание и заполнение структур для ЗАДАЧИ 2
 CREATE TABLE test
     (n NUMBER(10) PRIMARY KEY);
 /
-COMMENT ON COLUMN test.n
-   IS 'Порядковый номер';
-/   
 CREATE TABLE test_log
     (n NUMBER(10) PRIMARY KEY);
-/
-COMMENT ON COLUMN test_log.n
-   IS 'Порядковый номер';
 /
 INSERT INTO test_log VALUES (8);
 /
